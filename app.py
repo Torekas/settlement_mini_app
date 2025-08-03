09e06aab601bb1a0be121c8974fd8cfb98152404
Torekas <janw-1990@wp.pl>
2025-08-03 18:18:15 +0200

    reapring the libs problem with matplotlib

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import base64
 from datetime import datetime
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 load_dotenv()
 app = Flask(__name__)
