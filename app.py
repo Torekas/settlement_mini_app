6125dd3d579767a5adbd85a893f59c5f724a58de
Torekas <janw-1990@wp.pl>
2025-08-10 14:22:44 +0200

    Repairing error with decimals

--- app.py
+++ app.py
@@ -19,19 +19,7 @@ MONGODB_URI = os.getenv(
     "mongodb+srv://janmichalak78:W9DJ4jAcjjVXFYPp@cluster0.gk8zvxq.mongodb.net/settlement_db?retryWrites=true&w=majority"
 )
 client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
-
-
-
-def get_db():
-    global client
-    if client is None:
-        client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
-    return client.settlement_db
-
-# everywhere you used `db = client['settlement_db']`,
-# instead do:
-db = get_db()
-# db = client['settlement_db']
+db = client['settlement_db']
 transactions_col = db['transactions']
 users_col = db['users']
 
--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,4 @@ from app import app
 application = app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    # Listen on all interfaces so Render’s proxy can reach it
-    application.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
